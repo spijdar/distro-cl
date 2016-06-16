61b2442156d470750bf432e3958d21e7a1d11464
Hugh Perkins <hughperkins@gmail.com>
2016-06-16 09:05:12 +0800

    add libpng12-dev to installdeps

--- install-deps-nogui-ubuntu1604.sh
+++ install-deps-nogui-ubuntu1604.sh
@@ -1,5 +1,5 @@
 # This is targeted only at Ubuntu for now (and even then, might be a bit buggy, please feel free to submit issues for it)
 # It installs enough to run torch, nn, clnn, cltorch; but no gui components, ie no qt, no itorch, etc
 
-sudo apt-get install -y wget git gcc-4.9 g++-4.9 cmake cmake-curses-gui libffi-dev libblas-dev liblapack-dev gfortran libreadline-dev libprotobuf-dev protobuf-compiler
+sudo apt-get install -y wget git gcc-4.9 g++-4.9 cmake cmake-curses-gui libffi-dev libblas-dev liblapack-dev gfortran libreadline-dev libprotobuf-dev protobuf-compiler libpng12-dev
 
