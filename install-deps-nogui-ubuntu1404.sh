e9e32d416acd644e6df3656a9fa311d84e0119e6
Hugh Perkins <hughperkins@gmail.com>
2016-06-16 08:13:18 +0800

    add protobuf to installdeps, for loadcaffe library

--- install-deps-nogui-ubuntu1404.sh
+++ install-deps-nogui-ubuntu1404.sh
@@ -1,5 +1,5 @@
 # This is targeted only at Ubuntu for now (and even then, might be a bit buggy, please feel free to submit issues for it)
 # It installs enough to run torch, nn, clnn, cltorch; but no gui components, ie no qt, no itorch, etc
 
-sudo apt-get install -y wget git gcc g++ cmake cmake-curses-gui libffi-dev libblas-dev liblapack-dev gfortran libreadline-dev
+sudo apt-get install -y wget git gcc g++ cmake cmake-curses-gui libffi-dev libblas-dev liblapack-dev gfortran libreadline-dev libprotobuf-dev protobuf-compiler
 
