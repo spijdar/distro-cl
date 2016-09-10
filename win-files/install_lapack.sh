3f3664d5c151b33b09502e39107f43d647f26c1b
Hugh Perkins <hughperkins@gmail.com>
2016-09-10 20:36:47 +0100

    add common controls path

--- win-files/install_lapack.sh
+++ win-files/install_lapack.sh
@@ -11,6 +11,7 @@ cd build
 # mkdir -p "${BASE}/install/lua/5.1"
 mkdir -p "${BASE}/install/bin"
 # "C:\Program Files\CMake\bin\cmake" "-DCMAKE_INSTALL_PREFIX=${BASE}/install" -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=1 -DCMAKE_GNUtoMS=1 -DCMAKE_Fortran_COMPILER=/mingw64/bin/x86_64-w64-mingw32-gfortran.exe ..
+VS140COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\"
 "C:\Program Files\CMake\bin\cmake" "-DCMAKE_INSTALL_PREFIX=${BASE}/install" -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=1 -DCMAKE_GNUtoMS=1 "-DCMAKE_GNUtoMS_VCVARS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" -DCMAKE_Fortran_COMPILER=/mingw64/bin/x86_64-w64-mingw32-gfortran.exe ..
 make -j 8
 make install
