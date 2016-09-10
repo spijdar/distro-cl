4f1cbea2c27cb425f6cd8af6dc1f871e8e00c381
Hugh Perkins <hughperkins@gmail.com>
2016-09-10 21:03:13 +0100

    add reg.exe to the PATH

--- win-files/install_lapack.sh
+++ win-files/install_lapack.sh
@@ -2,7 +2,7 @@ echo SOFT $SOFT
 echo BASE $BASE
 echo DOWNLOADS $DOWNLOADS
 
-PATH=/mingw64/bin:/usr/bin
+export PATH=/mingw64/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/sbin:/bin:/c/Windows:/c/Windows/System32
 
 cd "${SOFT}"
 cd lapack-3.6.1
@@ -11,7 +11,8 @@ cd build
 # mkdir -p "${BASE}/install/lua/5.1"
 mkdir -p "${BASE}/install/bin"
 # "C:\Program Files\CMake\bin\cmake" "-DCMAKE_INSTALL_PREFIX=${BASE}/install" -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=1 -DCMAKE_GNUtoMS=1 -DCMAKE_Fortran_COMPILER=/mingw64/bin/x86_64-w64-mingw32-gfortran.exe ..
-VS140COMNTOOLS="C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\Tools\\"
+export COMMONPROGRAMFILES="C:\\Program Files\\Common Files"
+export VS140COMNTOOLS="C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\Tools\\"
 "C:\Program Files\CMake\bin\cmake" "-DCMAKE_INSTALL_PREFIX=${BASE}/install" -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=1 -DCMAKE_GNUtoMS=1 "-DCMAKE_GNUtoMS_VCVARS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" -DCMAKE_Fortran_COMPILER=/mingw64/bin/x86_64-w64-mingw32-gfortran.exe ..
 make -j 8
 make install
